db2f49f43426d2210ea35ac68920e29607a1f43f
Anthony <crayonno.o@gmail.com>
2023-09-03 05:47:13 -0700

    gsap fix

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,23 +1,67 @@
-import React from "react";
+"use client";
+
+import React, { useLayoutEffect, useRef } from "react";
 import Card from "@/components/Card";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const Categories = () => {
+  const ref = useRef(null);
+  const g = gsap.utils.selector(ref);
+  const t1 = gsap.timeline({});
+  gsap.registerPlugin(ScrollTrigger);
+  const flavors = ["", "", ""];
+
+  //gsap
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.fromTo(
+        "#group div",
+        {
+          y: 20,
+          opacity: 0.2,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: "#top",
+            start: "top 80%",
+            end: "bottom 75%",
+            scrub: 1,
+          },
+          stagger: {
+            each: 0.1,
+
+            ease: "power3.inOut",
+          },
+        }
+      );
+    });
+
+    return () => ctx.revert();
+  });
+
   return (
-    <div className="bg-black  pt-10 py-4 lg:px-20 px-6">
+    <div
+      ref={ref}
+      id="top"
+      className="bg-black  pt-10 py-4 lg:px-40 px-6 overflow-hidden"
+    >
       <div className="  my-8  gap-y-4">
         <h1 className="text-3xl font-bold text-secondary">Items</h1>
-        <p className="text-[white]">
+        <p className="text-[white] lg:max-w-[700px] ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
           suscipit beatae? Enim modi omnis adipisci officia perspiciatis est
           nesciunt quibusdam?
         </p>
       </div>
-      <div className="  grid grid-cols-2  gap-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div id={"group"} className="  grid grid-cols-2 lg:grid-cols-4 gap-4">
+        {flavors.map((e) => (
+          <div id="card">
+            <Card />
+          </div>
+        ))}
       </div>
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-[400px] px-5 lg:px-20 lg:h-screen bg-black w-full flex flex-col justify-center items-center pt-[100px]">
+    <div className="h-[400px] px-5 lg:px-60 lg:h-screen bg-black w-full flex flex-col justify-center items-center pt-[100px]">
       <div className="relative flex justify-center items-center">
         <img src="/cup.png" className="w-2/6 mt-10 " alt="" />
         <i className="w-[100px] absolute"></i>
       </div>
-      <div className="gap-y-4 flex flex-col w-full justify-center items-center">
-        <h1 className="capitalize text-4xl font-extrabold text-secondary ">
+      <div className="gap-y-4 flex flex-col w-full lg:gap-y-6 justify-center items-center">
+        <h1 className="capitalize text-4xl lg:text-7xl font-extrabold text-secondary ">
           Get a cup of coffee
         </h1>
         <p className="text-white text-center text-md">
@@ -16,13 +16,13 @@ const Hero = () => {
           obcaecati consequuntur voluptates voluptas sint deleniti accusantium
           eos a repudiandae odio.
         </p>
-        <button className="bg-secondary rounded-md text-white flex justify-center items-center gap-x-4 w-full h-[50px]  text-2xl">
+        <button className="bg-secondary lg:w-[400px] rounded-md text-white flex justify-center items-center gap-x-4 w-full h-[50px]  text-2xl">
           <i className="bi bi-cart"></i>
           <p className="text-xl">Buy Now</p>
         </button>
 
         <div className="w-full inline-flex justify-center items-center ">
-          <div className="text-secondary text-xl relative bg-[#1D1D1D] rounded-full py-1 px-2">
+          <div className="text-secondary text-xl  relative bg-[#1D1D1D] rounded-full py-1 px-2">
             <i className="bi bi-cup-hot-fill absolute animate-ping"></i>
             <i className="bi bi-cup-hot-fill "></i>
           </div>
